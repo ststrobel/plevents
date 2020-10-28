e7258331d0f84e285640d579d9400a5e0b478ade
Stefan Strobel <stefa.1@web.de>
2020-10-28 18:19:12 +0100

    removed adyen integration

--- backend/src/controllers/payment.controller.ts
+++ backend/src/controllers/payment.controller.ts
@@ -2,9 +2,6 @@ import * as express from 'express';
 import { ROLE } from '../../../common/tenant-relation';
 import tenantCorrelationHandler from '../handlers/tenant-correlation-handler';
 import { Subscription } from '../models/subscription';
-import { PaymentService } from '../services/payment.service';
-const dotenv = require('dotenv');
-dotenv.config();
 
 export class PaymentController {
   public static register(app: express.Application): void {
@@ -25,38 +22,8 @@ export class PaymentController {
       '/secure/tenants/:tenantId/subscriptions',
       tenantCorrelationHandler(ROLE.OWNER),
       async (request, response) => {
-        const paymentLink = await PaymentService.get().initiate(
-          request.params.tenantId,
-          request.body.months
-        );
-        response.status(200).send({ paymentLink });
+        response.status(400).send({ error: 'not implemented' });
       }
     );
-
-    // this will handle the webhook callback from adyen
-    app.post('/payment/callback', async (request, response) => {
-      // check that the request comes from adyen by checking the basic auth credentials
-      if (
-        request.get('Authorization') &&
-        request.get('Authorization').startsWith('Basic ')
-      ) {
-        const base64Credentials = request.get('Authorization').split(' ')[1];
-        const credentials = Buffer.from(base64Credentials, 'base64').toString(
-          'ascii'
-        );
-        if (
-          credentials ===
-          `${process.env.ADYEN_USER}:${process.env.ADYEN_PASSWORD}`
-        ) {
-          // request is authorized, continue with the business logic to active a tenant
-          PaymentService.get().handleCallback(request.body);
-          response.status(202).send('[accepted]');
-        } else {
-          response.sendStatus(401);
-        }
-      } else {
-        response.sendStatus(401);
-      }
-    });
   }
 }
